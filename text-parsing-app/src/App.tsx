233e84cb29fe76bca95b44f530d253349044abeb
mellio <kllmmc23@gmail.com>
2023-10-18 06:39:43 -0600

    add css initial set up

--- text-parsing-app/src/App.tsx
+++ text-parsing-app/src/App.tsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
+    <div className="container">
       <h1>Hello World</h1>
-    </>
-  )
+      <form>
+        <input />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
