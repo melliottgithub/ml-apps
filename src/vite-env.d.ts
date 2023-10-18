fc15d351821183f7f8e1db9194df7c8df8644b89
mellio <kllmmc23@gmail.com>
2023-10-17 22:20:38 -0600

    add cicd test 5

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
+/// <reference types="./vite-env-override.d.ts" />
+/// <reference types="vite/client" />
 declare module '*.svg' {
     const content: React.FC<React.SVGProps<SVGElement>>
     export default content
 }
-/// <reference types="./vite-env-override.d.ts" />
-
-/// <reference types="vite/client" />
