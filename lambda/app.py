fee6ce29ab08bff8866e27d411925560ff720465
mellio <kllmmc23@gmail.com>
2023-10-19 10:43:35 -0600

    add parse function

--- lambda/app.py
+++ lambda/app.py
@@ -7,16 +7,23 @@ from nltk.stem import PorterStemmer
 nltk.download('stopwords')
 ps = PorterStemmer()
 
-def lambda_handler(event, context):
+def parse(ps, params):
+    text = params['text']
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
+    text = text.split() ## add tokenizer from nltk library
+    stop_words = set(stopwords.words('english'))
+    tokens = [ps.stem(word) for word in text if word not in stop_words]
+    return ' '.join(tokens)
+
+def lambda_handler(event, context):    
+    params = event.get('body')
+
     try:
-        text = event['text']
-        text = text.lower()
-        text = re.sub(r'[^a-zA-Z]', ' ', text)
-        text = text.split()
-        stop_words = set(stopwords.words('english'))
-        tokens = [ps.stem(word) for word in text if word not in stop_words]
-        parsed_text = ' '.join(tokens)
-        
+        if type(params) == str:
+            params = json.loads(event.get('body'))
+
+        parsed_text = parse(ps, params)
         response = {'parsed_text': parsed_text}
         return {
             'statusCode': 200,
