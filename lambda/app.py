33ee6b33c03d6d2184b331dc9aba41d3f2949224
mellio <kllmmc23@gmail.com>
2023-10-30 03:31:40 -0400

    add most common words and wordcloud

--- lambda/app.py
+++ lambda/app.py
@@ -1,42 +1,79 @@
-import json
-import re
+# import json
+# import re
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk.stem import PorterStemmer
+
+# nltk.download('stopwords')
+# ps = PorterStemmer()
+
+# def parse(ps, params):
+#     text = params['text']
+#     text = text.lower()
+#     text = re.sub(r'[^a-zA-Z]', ' ', text)
+#     text = text.split() ## add tokenizer from nltk library
+#     stop_words = set(stopwords.words('english'))
+#     tokens = [ps.stem(word) for word in text if word not in stop_words]
+#     return ' '.join(tokens)
+
+# def lambda_handler(event, context):    
+#     params = event.get('body')
+
+#     try:
+#         if type(params) == str:
+#             params = json.loads(event.get('body'))
+
+#         parsed_text = parse(ps, params)
+#         response = {'parsed_text': parsed_text}
+#         return {
+#             'headers': {
+#                 'Access-Control-Allow-Origin': '*'
+#                 },
+#             'statusCode': 200,
+#             'body': json.dumps(response)
+#         }
+#     except Exception as ex:
+#         return {
+#             'headers': {
+#                 'Access-Control-Allow-Origin': '*'
+#                 },
+#             'statusCode': 500,
+#             'body': f'Internal error: {str(ex)}'
+#         }
+
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-
+nltk.download('punkt')
 nltk.download('stopwords')
-ps = PorterStemmer()
+nltk.download('wordnet')
 
-def parse(ps, params):
-    text = params['text']
-    text = text.lower()
-    text = re.sub(r'[^a-zA-Z]', ' ', text)
-    text = text.split() ## add tokenizer from nltk library
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+def text_parse_nltk(text, lemmatize=False, keep_numbers=False):
+    text = re.sub(r'[^a-zA-Z\s]' if not keep_numbers else r'[^a-zA-Z0-9\s]', '', text.lower())
+    words = word_tokenize(text)
+
     stop_words = set(stopwords.words('english'))
-    tokens = [ps.stem(word) for word in text if word not in stop_words]
-    return ' '.join(tokens)
 
-def lambda_handler(event, context):    
-    params = event.get('body')
+    processor = WordNetLemmatizer() if lemmatize else PorterStemmer()
 
+    result_tokens = [processor.lemmatize(word) if lemmatize else processor.stem(word) for word in words if word not in stop_words]
+    
+    return ' '.join(result_tokens)
+
+def lambda_handler(event, context):
     try:
-        if type(params) == str:
-            params = json.loads(event.get('body'))
-
-        parsed_text = parse(ps, params)
-        response = {'parsed_text': parsed_text}
+        input_text = event.get('text')
+        processed_text = text_parse_nltk(input_text, lemmatize=True, keep_numbers=False)
+        response = {'parsed_text': processed_text}
         return {
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
-                },
             'statusCode': 200,
             'body': json.dumps(response)
         }
     except Exception as ex:
         return {
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
-                },
             'statusCode': 500,
             'body': f'Internal error: {str(ex)}'
         }
